bcdf02f166b6e0fc1a4e74a942833a3b4619d239
Heitor Belem <heitorlbelem98@gmail.com>
2024-08-30 17:04:49 -0300

    Move theme provider to App component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,20 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
 
 export function Router() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Routes>
-        <Route element={<Header />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-      </Routes>
-
-      <GlobalStyle />
-    </ThemeProvider>
+    <Routes>
+      <Route element={<Header />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
