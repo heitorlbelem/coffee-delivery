eece53004da1ef6df8603491695437e85de54200
Heitor Belem <heitorlbelem98@gmail.com>
2024-09-02 10:46:27 -0300

    Extract banner to new component

---
+++ src/pages/Home/Banner/index.tsx
@@ -0,0 +1,37 @@
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
+import coffeePrincipal from "../../../assets/images/principal.png";
+import { defaultTheme } from "../../../styles/themes/default";
+import { BannerContainer, FeatureItem, FeaturesContainer } from "./styles";
+
+export function Banner() {
+  return (
+    <BannerContainer>
+      <div>
+        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+        <p>
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora
+        </p>
+        <FeaturesContainer>
+          <FeatureItem featureColor={defaultTheme.yellow}>
+            <ShoppingCart size={14} weight="fill" />
+            Compra simples e segura
+          </FeatureItem>
+          <FeatureItem featureColor={defaultTheme["base-text"]}>
+            <Package size={14} weight="fill" />
+            Embalagem mantém o café intacto
+          </FeatureItem>
+          <FeatureItem featureColor={defaultTheme["yellow-dark"]}>
+            <Timer size={14} weight="fill" />
+            Entrega rápida e rastreada
+          </FeatureItem>
+          <FeatureItem featureColor={defaultTheme.purple}>
+            <Coffee size={14} weight="fill" />O café chega fresquinho até você
+          </FeatureItem>
+        </FeaturesContainer>
+      </div>
+
+      <img src={coffeePrincipal} alt="" />
+    </BannerContainer>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,51 +1,19 @@
-import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
-import coffeePrincipal from "../../assets/images/principal.png";
-import { defaultTheme } from "../../styles/themes/default";
+import { ShoppingCart } from "phosphor-react";
 import {
-  BannerContainer,
   CardsGrid,
   CoffeeCard,
-  FeatureItem,
-  FeaturesContainer,
   ProductsContainer,
   ShoppingCartButton,
 } from "./styles";
 
 import traditionalCoffee from "../../assets/images/traditional.png";
 import { CountButton } from "../../components/CountButton";
+import { Banner } from "./Banner";
 
 export function Home() {
   return (
     <>
-      <BannerContainer>
-        <div>
-          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-          <p>
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </p>
-          <FeaturesContainer>
-            <FeatureItem featureColor={defaultTheme.yellow}>
-              <ShoppingCart size={14} weight="fill" />
-              Compra simples e segura
-            </FeatureItem>
-            <FeatureItem featureColor={defaultTheme["base-text"]}>
-              <Package size={14} weight="fill" />
-              Embalagem mantém o café intacto
-            </FeatureItem>
-            <FeatureItem featureColor={defaultTheme["yellow-dark"]}>
-              <Timer size={14} weight="fill" />
-              Entrega rápida e rastreada
-            </FeatureItem>
-            <FeatureItem featureColor={defaultTheme.purple}>
-              <Coffee size={14} weight="fill" />O café chega fresquinho até você
-            </FeatureItem>
-          </FeaturesContainer>
-        </div>
-
-        <img src={coffeePrincipal} alt="" />
-      </BannerContainer>
-
+      <Banner />
       <ProductsContainer>
         <h2>Nossos cafés</h2>
         <CardsGrid>
