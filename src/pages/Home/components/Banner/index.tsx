177c9ad3f766c139a56a78ffcf8b43ab2f2c6145
Heitor Belem <heitorlbelem98@gmail.com>
2024-09-03 17:49:44 -0300

    Refactoring banner component to improve the readability

--- src/pages/Home/components/Banner/index.tsx
+++ src/pages/Home/components/Banner/index.tsx
@@ -1,37 +1,43 @@
 import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
-import coffeePrincipal from "../../../../assets/images/principal.png";
-import { defaultTheme } from "../../../../styles/themes/default";
-import { BannerContainer, FeatureItem, FeaturesContainer } from "./styles";
+import coffeeMainImage from "../../../../assets/images/principal.png";
+import {
+  BannerHeader,
+  BannerWrapper,
+  FeatureItem,
+  FeaturesList,
+} from "./styles";
 
 export function Banner() {
   return (
-    <BannerContainer>
+    <BannerWrapper>
       <div>
-        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-        <p>
-          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-          hora
-        </p>
-        <FeaturesContainer>
-          <FeatureItem featureColor={defaultTheme.yellow}>
+        <BannerHeader>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p>
+            Com o Coffee Delivery, você recebe seu café onde estiver, a qualquer
+            hora.
+          </p>
+        </BannerHeader>
+        <FeaturesList>
+          <FeatureItem featureColor="yellow">
             <ShoppingCart size={14} weight="fill" />
             Compra simples e segura
           </FeatureItem>
-          <FeatureItem featureColor={defaultTheme["base-text"]}>
+          <FeatureItem featureColor="gray">
             <Package size={14} weight="fill" />
             Embalagem mantém o café intacto
           </FeatureItem>
-          <FeatureItem featureColor={defaultTheme["yellow-dark"]}>
+          <FeatureItem featureColor="yellow-dark">
             <Timer size={14} weight="fill" />
             Entrega rápida e rastreada
           </FeatureItem>
-          <FeatureItem featureColor={defaultTheme.purple}>
+          <FeatureItem featureColor="purple">
             <Coffee size={14} weight="fill" />O café chega fresquinho até você
           </FeatureItem>
-        </FeaturesContainer>
+        </FeaturesList>
       </div>
 
-      <img src={coffeePrincipal} alt="" />
-    </BannerContainer>
+      <img src={coffeeMainImage} alt="Imagem principal do café" />
+    </BannerWrapper>
   );
 }
--- src/pages/Home/components/Banner/styles.ts
+++ src/pages/Home/components/Banner/styles.ts
@@ -1,33 +1,33 @@
 import styled from "styled-components";
 
-export const BannerContainer = styled.section`
+export const BannerWrapper = styled.section`
   display: flex;
   align-items: flex-start;
   justify-content: space-between;
 
   margin-top: 9.4rem;
 
-  & > div {
-    max-width: 60rem;
-    h1 {
-      font: ${(props) => props.theme["title-xl"]};
-      color: ${(props) => props.theme["base-title"]};
-    }
-
-    p {
-      margin-top: 1.6rem;
-
-      font: ${(props) => props.theme["text-lg"]};
-      color: ${(props) => props.theme["base-subtitle"]};
-    }
-  }
-
   img {
     width: 47.6rem;
   }
 `;
 
-export const FeaturesContainer = styled.div`
+export const BannerHeader = styled.header`
+  max-width: 60rem;
+  h1 {
+    font: ${(props) => props.theme["title-xl"]};
+    color: ${(props) => props.theme["base-title"]};
+  }
+
+  p {
+    margin-top: 1.6rem;
+
+    font: ${(props) => props.theme["text-lg"]};
+    color: ${(props) => props.theme["base-subtitle"]};
+  }
+`;
+
+export const FeaturesList = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   row-gap: 2rem;
@@ -35,8 +35,15 @@ export const FeaturesContainer = styled.div`
   margin-top: 6.6rem;
 `;
 
+const FEATURE_COLORS = {
+  yellow: "yellow",
+  "yellow-dark": "yellow-dark",
+  purple: "purple",
+  gray: "base-title",
+} as const;
+
 interface FeatureItemProps {
-  featureColor: string;
+  featureColor: keyof typeof FEATURE_COLORS;
 }
 
 export const FeatureItem = styled.span<FeatureItemProps>`
@@ -53,7 +60,8 @@ export const FeatureItem = styled.span<FeatureItemProps>`
     padding: 0.4rem;
     border-radius: 999px;
 
-    background-color: ${(props) => props.featureColor};
+    background-color: ${(props) =>
+      props.theme[FEATURE_COLORS[props.featureColor]]};
     color: ${(props) => props.theme.white};
   }
 `;
